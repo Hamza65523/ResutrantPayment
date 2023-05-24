b5a5bca3af12e63c55d6716fa27c527263b512eb
Hamza65523 <mhamzaking2020@gmail.com>
2023-05-24 12:10:13 +0500

    first commit

---
+++ backend/products.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    id: 1,
+    name: "Biryani",
+    desc: "THis is best biryani",
+    price: 999,
+    image:
+      "https://static.toiimg.com/thumb/54308405.cms?width=1200&height=900",
+  },
+  {
+    id: 2,
+    name: "Pulao",
+    desc: "THis is best pualo",
+    price: 699,
+    image:
+      "https://www.indianveggiedelight.com/wp-content/uploads/2019/07/veg-pulao-featured.jpg",
+  },
+  {
+    id: 3,
+    name: "Chicken biryani",
+    desc: "THis is best chicken biryani",
+    price: 399,
+    image:
+      "https://static.toiimg.com/thumb/54308405.cms?width=1200&height=900",
+  },
+];
+
+module.exports = products;
